d021fc0b85719437a7e1fc3da040e717b14628ae
helen-cho <hd9536@daum.net>
2025-05-12 20:23:50 +0900

    2025-05-12

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const basename = process.env.PUBLIC_URL;
 
 root.render(
-    <HashRouter>
+    <BrowserRouter basename={basename}>
         <App/>
-    </HashRouter>
+    </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
