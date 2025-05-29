5879d6ba2a047ab5de2ea9ba872590a36361f83c
helen-cho <58962560+helen-cho@users.noreply.github.com>
2025-05-29 11:58:26 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCuNSRY2fxWJujedGR7upyp3b_GHG2c7lA",
-  authDomain: "inha-46136.firebaseapp.com",
-  projectId: "inha-46136",
-  storageBucket: "inha-46136.firebasestorage.app",
-  messagingSenderId: "533804388879",
-  appId: "1:533804388879:web:c8bfc7513927d87d46897a",
-  measurementId: "G-C1PWWJV0PQ"
+
 };
 
 // Initialize Firebase
